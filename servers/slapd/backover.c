d94393903a9b33fdfc9cd41b81cc0c7423953a2d
Josh Soref <jsoref@users.noreply.github.com>
2017-02-26 06:29:41 +0000

    spelling: instantiated

--- servers/slapd/backover.c
+++ servers/slapd/backover.c
@@ -1109,7 +1109,7 @@ overlay_register_control( BackendDB *be, const char *oid )
 				gotit = 1;
 			}
 
-			/* overlays can be instanciated multiple times, use
+			/* overlays can be instantiated multiple times, use
 			 * be_ctrls[ cid ] as an instance counter, so that the
 			 * overlay's controls are only really disabled after the
 			 * last instance called overlay_register_control() */
@@ -1120,7 +1120,7 @@ overlay_register_control( BackendDB *be, const char *oid )
 	}
 	
 	if ( !gotit ) {
-		/* overlays can be instanciated multiple times, use
+		/* overlays can be instantiated multiple times, use
 		 * be_ctrls[ cid ] as an instance counter, so that the
 		 * overlay's controls are only really unregistered after the
 		 * last instance called overlay_register_control() */
