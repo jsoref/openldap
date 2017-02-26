348cb0b2f0972cb2c19b0eeeeee7d95134a3660d
Josh Soref <jsoref@users.noreply.github.com>
2017-02-26 05:26:33 +0000

    spelling: buffered

--- contrib/slapd-modules/nssov/nss-pam-ldapd/tio.c
+++ contrib/slapd-modules/nssov/nss-pam-ldapd/tio.c
@@ -422,7 +422,7 @@ int tio_write(TFILE *fp, const void *buf, size_t count)
   uint8_t *tmp;
   size_t newsz;
   const uint8_t *ptr = (const uint8_t *)buf;
-  /* keep filling the buffer until we have bufferred everything */
+  /* keep filling the buffer until we have buffered everything */
   while (count > 0)
   {
     /* figure out free size in buffer */
